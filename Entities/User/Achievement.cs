418f0806b2371202041550717f4692942f04aae4
Nikita Pankiv <nikita.pankiv@techmagic.co>
2017-10-16 16:34:25 +0300

    User Entity

---
+++ Entities/User/Achievement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyUkr.DataAccessLayer.Entities.User
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string ImageAdress { get; set; }
+        public virtual ICollection<User> GainedUsers { get; set; }= new HashSet<User>();
+    }
+}
