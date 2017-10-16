738b1aeca82febcdf1f750aed26ae6524c2e2849
Nikita Pankiv <nikita.pankiv@techmagic.co>
2017-10-16 14:10:20 +0300

    Added dialogue

---
+++ Entities/Dialogue/Dialogue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyUkr.DataAccessLayer.Entities.Dialogue
+{
+    public class Dialogue
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string DialogueUkr { get; set; }
+        public string DialogueEng { get; set; }
+        public string FileAdress { get; set; }
+    }
+}
