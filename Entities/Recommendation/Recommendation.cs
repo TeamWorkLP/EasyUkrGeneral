293174ce63dd4a82cee1efe49edce8207981361b
Nikita Pankiv <nikita.pankiv@techmagic.co>
2017-10-16 15:56:08 +0300

    Improved recommendations

---
+++ Entities/Recommendation/Recommendation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyUkr.DataAccessLayer.Entities.Recommendation
+{
+    public class Recommendation
+    {
+        public int Id { get; set; }
+        public string HeaderUkr { get; set; }
+        public string TranslateEng { get; set; }
+        public string UrlLink { get; set; }
+        public string FileAdress { get; set; }
+        [Required]
+        public RecommendationCategory ParentCategory { get; set; }
+    }
+}
