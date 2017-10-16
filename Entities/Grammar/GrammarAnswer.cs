c3461bb99e4f1ee580f7f7d290cecf61016e2f58
Nikita Pankiv <nikita.pankiv@techmagic.co>
2017-10-16 15:20:55 +0300

    Added grammar

---
+++ Entities/Grammar/GrammarAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyUkr.DataAccessLayer.Entities.Grammar
+{
+    public class GrammarAnswer
+    {
+        public int Id { get; set; }
+        public string Answer { get; set; }
+        public bool? IsCorrect { get; set; }
+        [Required]
+        public virtual GrammarTask GrammarTask { get; set; }
+    }
+}
---
+++ Entities/Grammar/GrammarTask.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyUkr.DataAccessLayer.Entities.Grammar
+{
+    public class GrammarTask
+    {
+        public int Id { get; set; }
+        public string HeaderUkr { get; set; }
+        public string TranslateEng { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<GrammarAnswer> GrammarAnswers { get; set; } = new HashSet<GrammarAnswer>();
+        [Required]
+        public virtual GrammarTopic GrammarTopic { get; set; }
+    }
+}
