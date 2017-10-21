650c7026891ea9c09bb2edae04b3d03f975debb1
DESKTOP-TF7PTTL\Dmytro <nicholasmakkalan@gmail.com>
2017-10-21 12:22:32 +0300

    created static path db manager with EF

---
+++ Server/EasyUkr.WebApi/Infrastructure/IDbManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using EasyUkr.DataAccessLayer.Contexts;
+
+namespace EasyUkr.WebApi.Infrastructure
+{
+    public interface IDbManager : IDisposable
+    {
+        EasyUkrDbContext Data { get; }
+        void SaveChanges();
+    }
+}
